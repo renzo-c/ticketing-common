bd716ac9383083e35f984ba6567c5c9106fc35d0
Renzo Navarro <rcnavarrop@gmail.com>
2023-07-29 14:13:28 -0400

    Additional package settings

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+interface Color {
+  red: number;
+  blue: number;
+  black: number;
+}
+
+const color: Color = {
+  red: 20,
+  blue: 20,
+  black: 20,
+};
+
+console.log(color);
+
+export default { color };
