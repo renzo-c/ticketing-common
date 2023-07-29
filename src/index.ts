d6662201cfd7794dbde595782de91ac9a7bbce6f
Renzo Navarro <rcnavarrop@gmail.com>
2023-07-29 15:17:21 -0400

    Create error definition and handlers, and middlewares

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,16 @@
-interface Color {
-  red: number;
-  blue: number;
-  black: number;
-}
+/**
+ * Importing everything from each file to export it again. 
+ * This will make easier for people who use the library to directly 
+ * import each error handler/middleware without specifying the whole path
+ */
+export * from "./errors/bad-request-error";
+export * from "./errors/custom-error"
+export * from "./errors/database-connection-error";
+export * from "./errors/not-authorized-error";
+export * from "./errors/not-found-error";
+export * from "./errors/request-validation-error";
 
-const color: Color = {
-  red: 20,
-  blue: 20,
-  black: 20,
-};
-
-console.log(color);
-
-export default { color };
+export * from "./middlewares/current-user";
+export * from "./middlewares/error-handlers";
+export * from "./middlewares/require-auth";
+export * from "./middlewares/validate-requests";
